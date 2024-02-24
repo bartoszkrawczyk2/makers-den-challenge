fdae78ed96515ae661a85d51ae8f2c49b048851b
Bartosz Krawczyk <bartoszkrawczyk2@gmail.com>
2024-02-24 17:22:12 +0100

    control dropdown visibility with state, not css

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
+import type { ChangeEvent, KeyboardEvent } from "react";
 import { useDebounced } from "../../utils/useDebounced";
+
+const MIN_SEARCH_LENGTH = 3;
 
 type SuggestionItem = {
   id: string | number;
@@ -13,20 +16,34 @@ type AutocompleteProps<T> = {
 export const Autocomplete = <T extends SuggestionItem>({
   asyncData,
 }: AutocompleteProps<T>) => {
+  // TODO: too much state, probably should use useReducer
   const [inputValue, setInputValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
   const [suggestions, setSuggestions] = useState<T[]>();
   const search = useDebounced(inputValue);
 
   useEffect(() => {
     const query = search.trim();
-    if (query.length < 3) return;
+    if (query.length < MIN_SEARCH_LENGTH) return;
 
     setIsLoading(true);
     asyncData?.(query)
       .then(setSuggestions)
       .finally(() => setIsLoading(false));
   }, [asyncData, search]);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    setIsVisible(true);
+  };
+
+  const handleKeyboard = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === "Escape") setIsVisible(false);
+  };
+
+  const shouldShowDropdown =
+    isVisible && inputValue.trim().length >= MIN_SEARCH_LENGTH;
 
   return (
     <div className="relative">
@@ -37,16 +54,20 @@ export const Autocomplete = <T extends SuggestionItem>({
       )}
       <input
         value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        className="[&:focus+ul:not(:empty)]:block bg-slate-50 py-3 px-4 mb-2 block w-full border border-gray-300 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50"
+        onKeyUp={handleKeyboard}
+        onChange={onChange}
+        onBlur={() => setIsVisible(false)}
+        className="bg-slate-50 py-3 px-4 mb-2 block w-full border border-gray-300 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50"
       />
-      <ul className="hidden overflow-y-auto max-h-64 absolute inset-x-0 bg-slate-50 border border-gray-300 rounded-lg -mt-1">
-        {!suggestions?.length && isLoading && <li>loading...</li>}
-        {suggestions?.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-        {suggestions?.length === 0 && <li>no results</li>}
-      </ul>
+      {shouldShowDropdown && (
+        <ul className="empty:hidden overflow-y-auto max-h-64 absolute inset-x-0 bg-slate-50 border border-gray-300 rounded-lg -mt-1">
+          {!suggestions?.length && isLoading && <li>loading...</li>}
+          {suggestions?.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+          {suggestions?.length === 0 && <li>no results</li>}
+        </ul>
+      )}
     </div>
   );
 };
