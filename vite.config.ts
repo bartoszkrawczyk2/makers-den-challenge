7d4d4099c640f1ca8808a79f33928356231d33d2
Bartosz Krawczyk <bartoszkrawczyk2@gmail.com>
2024-02-24 13:24:25 +0100

    add env variable validation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  if (!env.VITE_GITHUB_TOKEN)
+    throw new Error("VITE_GITHUB_TOKEN env variable is missing");
+
+  return {
+    plugins: [react()],
+  };
+});
